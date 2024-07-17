b13310ff56c72a21c4d434ff17df4877ed727440
FJHGF <fjhgf@naver.com>
2024-07-17 19:20:03 +0900

    RecordDAO_bh : 메서드 생성

--- Tennis/src/com/java/tennis/model/RecordDTO.java
+++ Tennis/src/com/java/tennis/model/RecordDTO.java
@@ -1,9 +1,106 @@
 package com.java.tennis.model;
 
 public class RecordDTO {
+//	private int no;					//일련번호
+//	private String name;			//이름
+//	private CharacterDTO character;	//캐릭터
+//	private boolean type;			//단식/복식
+//	private MatchDTO match;			//매치데이터
+//	private TennisDTO tennis;		// 게임 점수
+	
+	
+	//---------------------------
+	//기록과 관련된 멤버 변수와 멤버메서드만 정리
 	private int no;					//일련번호
+	private String date;			//날짜
 	private String name;			//이름
-	private CharacterDTO character;	//캐릭터
-	private boolean type;			//단식/복식
-	private MatchDTO match;			//매치데이터
+//	private int characterno;		//캐릭터 번호
+	private int scoreme;			//스코어1(나)
+	private int socrecumputer;		//스코어2(컴퓨터)
+	
+	
+	private CharacterDTO character;	//캐릭터 > 캐릭터 번호 가져오기
+	private TennisDTO tennis;		// 게임 점수 > 세트 번호, 게임 번호 가져오기
+	private MatchDTO match;			//매치데이터 > 
+	
+	
+	// 일련번호, 날짜, 이름, 캐릭터 번호, 스코어1(나), 스코어2(컴퓨터)
+	// 1,2024-04-03,현영석,3,2,0
+	
+	// 일련번호, 세트 번호, 게임 번호, 스코어1(나), 스코어2(컴퓨터) 
+	// 1,1,1,4,0 
+	
+	
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScoreme() {
+		return scoreme;
+	}
+	public void setScoreme(int scoreme) {
+		this.scoreme = scoreme;
+	}
+	public int getSocrecumputer() {
+		return socrecumputer;
+	}
+	public void setSocrecumputer(int socrecumputer) {
+		this.socrecumputer = socrecumputer;
+	}
+	public CharacterDTO getCharacter() {
+		return character;
+	}
+	public void setCharacter(CharacterDTO character) {
+		this.character = character;
+	}
+	public TennisDTO getTennis() {
+		return tennis;
+	}
+	public void setTennis(TennisDTO tennis) {
+		this.tennis = tennis;
+	}
+	public MatchDTO getMatch() {
+		return match;
+	}
+	public void setMatch(MatchDTO match) {
+		this.match = match;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return String.format(
+				"RecordDTO [no=%s, date=%s, name=%s, scoreme=%s, socrecumputer=%s, character=%s, tennis=%s, match=%s]",
+				no, date, name, scoreme, socrecumputer, character, tennis, match);
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
