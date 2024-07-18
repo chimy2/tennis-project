4ca27479bb0b16d80e544a8405d5acc24f8d852c
Bristolparkway <sangsongsoo@gmail.com>
2024-07-18 19:53:22 +0900

    hahahahahahah

--- Tennis/src/com/java/tennis/service/TennisService.java
+++ Tennis/src/com/java/tennis/service/TennisService.java
@@ -150,24 +150,48 @@ public class TennisService {
 		countSet = 1;
 		
 		
-		
+		System.out.println();
 		System.out.println("게임을 시작합니다.");
 		boolean loop = true;
 		while (loop) {
 
+			pointCheck(dtoCharacter);
+			
+			if (dto.getSet() == 1) {
+				if (me.pointSet == 2 || cpu.pointSet == 2) {
+					break;
+				}
+			} else if (dto.getSet() == 2) {
+				if (me.pointSet == 3 || cpu.pointSet == 3) {
+					break;
+				}
+			}
+
+			System.out.println();
+			System.out.println("==============================================");
+			System.out.printf("%d세트 %d게임 %d회차 서브\r\n", countSet, countGame, countTotalServe);
+			System.out.println("==============================================");
+
 			AbilityDTO dtoAbility = new AbilityDTO();
 			int input;
 			GameView viewGame = new GameView();
-			viewGame.gameView(dtoCharacter);
-			input = scan.nextInt();
-			scan.skip("\r\n");
+			while (true) {
+				viewGame.gameView(dtoCharacter);
+				input = scan.nextInt();
+				scan.skip("\r\n");
+				if (input > 4 || input < 1) {
+					System.out.println("1번부터 4번까지의 기술 중 하나를 선택하세요.");
+					continue;
+				} else {
+					break;
+				}
+			}
+			
 			
 			Random rnd = new Random();
 			me.chance = rnd.nextInt(100) + 50 + dtoAbility.statModifier(input); //stats[i]
 			cpu.chance = rnd.nextInt(100) + 1;
 
-			System.out.println();
-			System.out.printf("%d세트 %d게임 %d회차 서브\r\n", countSet, countGame, countTotalServe);
 	
 			
 			if (me.chance > cpu.chance) {
@@ -188,39 +212,129 @@ public class TennisService {
 			System.out.println(me.point + "-" + cpu.point);
 			System.out.println("[확인]");
 			scan.nextLine();
+			System.out.println();
 			
 			
 		}
-		pointCheck();
-
-		}
-	
-	
-
-
-
-
-
-
-	private void pointCheck() {
 		
-		boolean loop = true;
-		while (loop) {
+		System.out.println("게임을 기록하시겠습니까?");
+		System.out.println("1.예	2.아니오");
+		scan.nextInt();
+		
+		
+	
+		}
+	
+	
+
+
+
+
+
+
+	private void pointCheck(CharacterDTO dtoCharacter) {
+		
+		String p1 = dtoCharacter.getName();
+		String p2 = "Player2";
+		
+		
+		
+
+		
+		
+		
+		
 		if (me.point > 3 && me.point - cpu.point > 1) {
-			
-			
 			me.pointGame++;
-			break;
+			me.point = 0;
+			cpu.point = 0;
+			System.out.println();
+			System.out.printf("%d세트 %d게임의 승자는 %s입니다.\r\n", countSet, countGame, p1);
+			System.out.printf("현재 게임 스코어는 [%d-%d] 입니다.\r\n", me.pointGame, cpu.pointGame);
+			System.out.println("다음 게임을 시작합니다.");
+			System.out.println("[확인]");
+			countGame++;
+			countTotalServe = 1;
+			scan.nextLine();
 		}
 		
 		if (cpu.point > 3 && cpu.point - me.point > 1) {
-			
 			cpu.pointGame++;
-			break;
+			me.point = 0;
+			cpu.point = 0;
+			System.out.println();
+			System.out.printf("%d세트 %d게임의 승자는 %s입니다.\r\n", countSet, countGame, p2);
+			System.out.printf("현재 게임 스코어는 [%d-%d] 입니다.\r\n", me.pointGame, cpu.pointGame);
+			System.out.println("다음 게임을 시작합니다.");
+			System.out.println("[확인]");
+			countGame++;
+			countTotalServe = 1;
+			scan.nextLine();
 		}
 		
-	}
+
+		if (me.pointGame > 5 && me.pointGame - cpu.pointGame > 1) {
+			
+			me.pointSet++;
+
+			me.point = 0;
+			me.pointGame = 0;
+			cpu.point = 0;
+			cpu.pointGame = 0;
+			System.out.println();
+			System.out.printf("%d세트의 승자는 %s입니다.\r\n", countSet, p1);
+			System.out.printf("현재 세트 스코어는 [%d-%d] 입니다.\r\n", me.pointSet, cpu.pointSet);
+			System.out.println("다음 세트를 시작합니다.");
+			System.out.println("[확인]");
+			countServe = 1;
+			countGame = 1;
+			countSet++;
+			scan.nextLine();
+			
+			
+		} else if (cpu.pointGame > 5 && cpu.pointGame - me.pointGame > 1) {
+			
+			cpu.pointSet++;
+
+			me.point = 0;
+			me.pointGame = 0;
+			cpu.point = 0;
+			cpu.pointGame = 0;
+			System.out.println();
+			System.out.printf("%d세트의 승자는 %s입니다.\r\n", countSet, p2);
+			System.out.printf("현재 세트 스코어는 [%d-%d] 입니다.\r\n", me.pointSet, cpu.pointSet);
+			System.out.println("다음 세트를 시작합니다.");
+			System.out.println("[확인]");
+			countServe = 1;
+			countGame = 1;
+			countSet++;
+			scan.nextLine();
+			
+		}
 		
+		if (dto.getSet() == 1) {
+			if (me.pointSet > 1) {
+				System.out.println();
+				System.out.printf("%s이(가) [%d-%d]로 매치를 승리했습니다.\r\n", p1, me.pointSet, cpu.pointSet);
+			} else if (cpu.pointSet > 1) {
+				System.out.println();
+				System.out.printf("%s이(가) [%d-%d]로 매치를 승리했습니다.\r\n", p2, cpu.pointSet, me.pointSet);
+			}
+		}
+		
+		if (dto.getSet() == 2) {
+			if (me.pointSet > 2) {
+				System.out.println();
+				System.out.printf("%s이(가) [%d-%d]로 매치를 승리했습니다.\r\n", p1, me.pointSet, cpu.pointSet);
+			} else if (cpu.pointSet > 2) {
+				System.out.println();
+				System.out.printf("%s이(가) [%d-%d]로 매치를 승리했습니다.\r\n", p2, cpu.pointSet, me.pointSet);
+			}
+		}
+		
+		
+		
+	
 	}
 
 
@@ -238,172 +352,6 @@ public class TennisService {
 		// TODO Auto-generated method stub
 		
 	}
-
-	
-}
-
-class TestingService {
-
-	private int chance;
-	private int point;
-	
-	private int pointGame;
-	private int pointSet;
-	
-	static int countServe;
-	static int countTotalServe;
-	static int countGame;
-	static int countSet;
-	
-	public static void main(String[] args) {
-
-//		m1();
-		m2();
-		
-	
-
-	
-	}//main
-
-
-private static void m2() {
-
-		Scanner scan = new Scanner(System.in);
-		TestingService me = new TestingService();
-		TestingService cpu = new TestingService();
-		
-		String p1 = "Player1";
-		String p2 = "Player2";
-		
-		countServe = 1;
-		countTotalServe = 1;
-		countGame = 1;
-		countSet = 1;
-
-		boolean loop = true;
-		while (loop) {
-			
-			
-			Random rnd = new Random();
-			me.chance = rnd.nextInt(100)+1+50;
-			cpu.chance = rnd.nextInt(100)+1;
-			
-			
-			//포인트 조건
-			System.out.println();
-			System.out.printf("%d세트 %d게임 %d회차 서브\r\n", countSet, countGame, countTotalServe);
-			
-			if (me.chance > cpu.chance) {
-				me.point++;
-				countTotalServe++;
-				countServe++;
-				System.out.println(p1 + " 득점!");
-			} else if (me.chance < cpu.chance) {
-				cpu.point++;
-				countTotalServe++;
-				countServe++;
-				System.out.println(p2 + " 득점!");			
-			} else {
-				continue;
-			}
-			System.out.println(me.point + "-" + cpu.point);
-			System.out.println("[확인]");
-			emptyScreen();
-			scan.nextLine();
-			
-			
-			//게임 포인트 조건
-			if (me.point > 3 && me.point - cpu.point > 1) {
-				me.pointGame++;
-				me.point = 0;
-				cpu.point = 0;
-				System.out.println();
-				System.out.printf("%d세트 %d게임의 승자는 %s입니다.\r\n", countSet, countGame, p1);
-				System.out.printf("현재 게임 스코어는 [%d-%d] 입니다.\r\n", me.pointGame, cpu.pointGame);
-				System.out.println("다음 게임을 시작합니다.");
-				System.out.println("[확인]");
-				emptyScreen();
-				countGame++;
-				countTotalServe = 1;
-				scan.nextLine();
-			} else if (cpu.point > 3 && cpu.point - me.point > 1) {
-				cpu.pointGame++;
-				me.point = 0;
-				cpu.point = 0;
-				System.out.println();
-				System.out.printf("%d세트 %d게임의 승자는 %s입니다.\r\n", countSet, countGame, p2);
-				System.out.printf("현재 게임 스코어는 [%d-%d] 입니다.\r\n", me.pointGame, cpu.pointGame);
-				System.out.println("다음 게임을 시작합니다.");
-				System.out.println("[확인]");
-				emptyScreen();
-				countGame++;
-				countTotalServe = 1;
-				scan.nextLine();
-			}
-			
-			//세트 포인트 조건
-			if (me.pointGame > 5 && me.pointGame - cpu.pointGame > 1) {
-				
-				me.pointSet++;
-
-				me.point = 0;
-				me.pointGame = 0;
-				cpu.point = 0;
-				cpu.pointGame = 0;
-				System.out.println();
-				System.out.printf("%d세트의 승자는 %s입니다.\r\n", countSet, p1);
-				System.out.printf("현재 세트 스코어는 [%d-%d] 입니다.\r\n", me.pointSet, cpu.pointSet);
-				System.out.println("다음 세트를 시작합니다.");
-				System.out.println("[확인]");
-				countServe = 1;
-				countGame = 1;
-				countSet++;
-				
-			} else if (cpu.pointGame > 5 && cpu.pointGame - me.pointGame > 1) {
-				
-				cpu.pointSet++;
-
-				me.point = 0;
-				me.pointGame = 0;
-				cpu.point = 0;
-				cpu.pointGame = 0;
-				System.out.println();
-				System.out.printf("%d세트의 승자는 %s입니다.\r\n", countSet, p2);
-				System.out.printf("현재 세트 스코어는 [%d-%d] 입니다.\r\n", me.pointSet, cpu.pointSet);
-				System.out.println("다음 세트를 시작합니다.");
-				System.out.println("[확인]");
-				countServe = 1;
-				countGame = 1;
-				countSet++;
-			}
-
-//			매치 포인트 조건
-			if (me.pointSet > minSetCheck()) {
-				
-			}
-			
-			
-			
-			
-		}
-		scan.close();
-	}
-
-
-
-	private static int minSetCheck() {
-
-//		if 
-
-	return 0;
-}
-
-
-	private static void emptyScreen() {
-		
-//		for (int i=0; i<30; i++) System.out.println();
-		
-}
 
 
 	private static void m1() {
--- Tennis/src/com/java/tennis/view/GameView.java
+++ Tennis/src/com/java/tennis/view/GameView.java
@@ -23,7 +23,7 @@ public class GameView {
 		} else if (i == 2) {
 			temp += "어떤 스킬을 사용하겠습니까?\r\n";
 			temp += "1. 서브\r\n";
-			temp += "2. 슈퍼포핸드!(\r\n";
+			temp += "2. 슈퍼포핸드!\r\n";
 			temp += "3. 백핸드\r\n";
 			temp += "4. 발리\r\n";
 			System.out.println(temp);
