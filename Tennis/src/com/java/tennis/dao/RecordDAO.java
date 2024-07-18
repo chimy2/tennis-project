3745e538bde7d97cfd6d8cb6ad15c745dfa74f6c
FJHGF <fjhgf@naver.com>
2024-07-19 08:53:02 +0900

    recordDATO 수정

--- Tennis/src/com/java/tennis/dao/RecordDAO.java
+++ Tennis/src/com/java/tennis/dao/RecordDAO.java
@@ -31,61 +31,7 @@ public class RecordDAO {
 
 	// 기록하는 메서드----------------------------------------------------------------
 
-	public boolean recordAdd(RecordDTO dto) {
-
-		// 간단한 점수 저장------------------------------------------
-		// 일련번호, 날짜, 이름, 캐릭터 번호, 스코어1(나), 스코어2(컴퓨터)
-		// 1,2024-04-03,현영석,3,2,0
-
-		try {
-
-			BufferedWriter writer = new BufferedWriter(new FileWriter(PATH + "record.txt", true)); // append모드로
-
-			String line = String.format("%s,%s,%s,%s,%s,%s\n", dto.getNo()// 일련번호
-					, dto.getDate() // 날짜
-//												,dto.getCharacter().getName() //유저 이름
-//												,dto.getCharacter().getNo() //캐릭터번호
-					, dto.getScoreme() // 스코어 1
-					, dto.getSocrecumputer());// 스코어 2
-
-			writer.write(line); // 점수-간단한 추가
-			writer.close(); // 파일 저장
-
-		} catch (Exception e) {
-			System.out.println("RecordDAO.record");
-			e.printStackTrace();
-		}
-
-		// 상세한 점수 저장------------------------------------------
-		// 일련번호, 세트번호, 게임번호, 스코어1, 스코어2
-		// 1,1,1,4,0
-
-		try {
-
-			BufferedWriter writerSpec = new BufferedWriter(new FileWriter(PATH + "game.txt", true)); // append모드로
-
-			String lineSpec = "";
-//							String.format("%s,%s,%s,%s,%s\n"
-//												,dto.getNo()// 일련번호
-////												,dto.getTennis().getSet() // 세트번호
-////												,dto.getTennis().getGamePoint() //게임번호
-//												,dto.getTennis().getPoint() //스코어 1
-//												,dto.getTennis().getPoint());// 스코어 2
-
-
-			writerSpec.write(lineSpec); // 점수-상세한 추가
-			writerSpec.close(); // 파일 저장
-
-		} catch (Exception e) {
-			System.out.println("RecordDAO.record");
-			e.printStackTrace();
-		}
-
-		System.out.println("\n 우승자 정보를 넣었습니다.");
-
-		return true; // 간단한 정보 저장되면 true
-	}
-
+	
 	// 정보 불러오는 메서드----------------------------------------------------------------
 
 	// 간단한 정보 불러오기
@@ -123,7 +69,7 @@ public class RecordDAO {
 
 			view.dividingLine(); // 구분선
 			System.out.println();
-
+			reader.close();
 		} catch (Exception e) {
 			System.out.println("RecordDAO.get");
 			e.printStackTrace();
