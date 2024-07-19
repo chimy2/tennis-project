376ab1501f527cc976ae550eef6af9e2f0521215
FJHGF <fjhgf@naver.com>
2024-07-19 13:43:12 +0900

    remerge

--- Tennis/src/com/java/tennis/service/RecordService.java
+++ Tennis/src/com/java/tennis/service/RecordService.java
@@ -1,7 +1,10 @@
 package com.java.tennis.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
+import com.github.lalyos.jfiglet.FigletFont;
 import com.java.tennis.dao.RecordDAO;
 import com.java.tennis.view.MainView;
 import com.java.tennis.view.RecordView;
@@ -20,13 +23,14 @@ public class RecordService {
 	}
 	
 	
-	public void get() {
+	public void get() { //명예의전당 출력
 		
-		view.title("명예의 전당"); // 길이가 안맞음
+		view.title("명예의 전당");
 		view.titleSpecific();// 간단한 정보 보여주는 메서드
 		dao.get();
-
+		
 	}
+	
 	
 	
 	public void getTotal() {
@@ -50,16 +54,17 @@ public class RecordService {
 			if (menu.equals("1")) {
 
 				sevice.get(); // 명예의 전당 출력
+				
+				MainView error = new MainView(); //계속하려면 엔터쳐라
+				error.pause();
 
 			} else if (menu.equals("2")) {
 				
 				String list = dao.recordSearch(dao.gameId());
-				if(list.equals("")) {
-					System.out.println("잘못된 아이디 입니다.");
+				if(list.equals("")) { //확인되지 않는 아이디 입력시 출력
+					System.out.println("확인되지 않는 아이디입니다.");
 					
-					System.out.println();
 					MainView error = new MainView();
-					error.errorInput();
 					error.pause();
 					
 
@@ -72,12 +77,14 @@ public class RecordService {
 
 			else if (menu.equals("3")) {
 
-				view.subTitle("최신 기록");
+				view.subMenu("최신 기록");
 				dao.getTotal(); // 전체 최신기록
+				//정렬질문
+				view.sortQuestion();
+				
 				System.out.println("1. 날짜");
 				System.out.println("2. 아이디");
 				System.out.println("3. 캐릭터");
-
 				System.out.println("번호 입력 : ");
 				String num = scan.nextLine();
 				if (num.equals("1") || num.equals("2") || num.equals("3")) {
