181ffe87d38a72c9da6f84d3b59fc260749416f1
bristolparkway <sangsongsoo@gmail.com>
2024-07-20 21:50:02 +0900

    hi

--- Tennis/src/com/java/tennis/view/GameView.java
+++ Tennis/src/com/java/tennis/view/GameView.java
@@ -37,6 +37,7 @@ public class GameView {
 //			result += "\r\n";
 //			
 //		}
+		
 		result += mainView.addMenuMarginCenter(
 				skillList.stream().map(s -> {
 					return skillName.equals(s.getName()) ?
